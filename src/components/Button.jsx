535d5cfeb5c415a586b98e8de6da997a3ffacf58
talhamunir401 <talhamunir401@gmail.com>
2024-09-14 00:53:04 +0500

    button fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function Button({children}) {
+function Button({ children, onClick }) {
   return (
-    <button style={{
-        padding:"10px 20px",
-        fontSize:"1.2em",
-        borderRadius:"5px",
-        cursor:'pointer',
-        backgroundColor:'blue',
-        color:'white',
-        border:'none'
-    }}>{children}</button>
-  )
+    <button
+      onClick={onClick}
+      style={{
+        padding: "10px 20px",
+        fontSize: "1.2em",
+        borderRadius: "5px",
+        cursor: "pointer",
+        backgroundColor: "blue",
+        color: "white",
+        border: "none",
+      }}
+    >
+      {children}
+    </button>
+  );
 }
 
-export  {Button};
+export { Button };
